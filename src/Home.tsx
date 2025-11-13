bb96027ba51a11d3d254b2849aeb1ae10b879216
RAFI MUHAMMAD MAHRUS <mahrusrafi@gmail.com>
2025-11-13 12:50:32 +0700

    adjust port

--- src/Home.tsx
+++ src/Home.tsx
@@ -483,7 +483,7 @@ function FlowCanvas() {
         finalPrompt = existingFlowSummary;
       }
       
-      const response = await fetch(`http://127.0.0.1:3000/api/generate/flow`, {
+      const response = await fetch(`http://127.0.0.1:5000/api/generate/flow`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
